ae6c540dc48846a00835e2b4bf94e7917d54cf3a
Sleepwalker423 <60832934+Sleepwalker423@users.noreply.github.com>
2024-12-17 03:53:20 -0500

    Update TestCases.java

--- inpututils/TestCases.java
+++ inpututils/TestCases.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+import inpututils.InputRequest;
+
+public class TestCases {
+
+    private static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+        int intValue = InputRequest.requestNumber("Enter int value.", 0, 10, scan);
+    }
+
+
+  
+        public void testMethod(){
+
+            double doubleValue = InputRequest.requestNumber("Enter double value.", 0.0, 4.0, scan);
+        }
+}
+
+
 
